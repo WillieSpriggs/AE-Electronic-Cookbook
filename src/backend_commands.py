1da474c35451eb6e152e0eae03850ffb57c7221d
Elijah Cox <elijah.thomas.cox@gmail.com>
2021-11-29 16:47:11 -0600

    add query execution, query print, init db check

--- src/backend_commands.py
+++ src/backend_commands.py
@@ -1,30 +1,57 @@
 import psycopg2
 
-#establishing the connection
-conn = psycopg2.connect(
-   database="aggieeats", user='postgres', password='admin', host='127.0.0.1', port= '5432'
-)
-conn.autocommit = True
+init_db = True
 
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
+def execute_query(cursor,command):
+    try:
+        cursor.execute(command)
+    except Exception as err:
+        print(err)
+    finally:
+        return cursor
 
-#Preparing query to create a database
-create_db = '''CREATE database AggieEats''';
-create_table = '''CREATE TABLE test1 (key INTEGER, value INTEGER)''';
-insert = '''INSERT INTO test1 (key,value) VALUES (5,6) ''';
-search = '''SELECT * FROM test1''';
+def connect_db(db_name):
+    try:
+        conn = psycopg2.connect(database=db_name, user='postgres', password='admin', host='127.0.0.1', port= '5432') #establishing the connection)
+        conn.autocommit = True
+        cursor = conn.cursor()
+    except Exception as err:
+        print(err)
+    finally:
+        return conn,cursor
+
+def print_query(cursor):
+    try:  
+        results = cursor.fetchall()
+        for r in results:
+            print(r)
+    except Exception as err:
+        print(err)
+    finally:
+        return
+
+conn,cursor = connect_db("postgres")
 
 try:
-    cursor.execute(insert)
-    cursor.execute(search)
-    results = cursor.fetchall()
-    for r in results:
-        print(r)
-except Exception as err:
-    print(err)
+    create_db = '''CREATE database aggieeats'''; #create the db
+    cursor.execute(create_db)
+    print("Database initialized")
+except psycopg2.errors.DuplicateDatabase:
+    print("Database already exists")
+    init_db = False
 finally:
-    pass
+    print("Closed initial DB")
+    conn.close() #close initial connection
 
+
+conn,cursor = connect_db("aggieeats")
+
+create_table = '''CREATE TABLE test2 (key INTEGER, value INTEGER)''';
+insert = '''INSERT INTO test2 (key,value) VALUES (5,6) ''';
+search = '''SELECT * FROM test1''';
+cursor = execute_query(cursor,create_table)
+cursor = execute_query(cursor,insert)
+cursor = execute_query(cursor,search)
+print_query(cursor)
 #Closing the connection
 conn.close()
