e342ad37f2a682ece0d75962351ca8e9a9ea9cfa
Elijah Cox <elijahtom@gmail.com>
2021-11-30 23:45:48 -0600

    add populate,drop db utils

--- src/backend_commands.py
+++ src/backend_commands.py
@@ -18,14 +18,23 @@ class backend:
         self.execute_query('CREATE database aggieeats;') #attempt to create aggieeats db, does nothing if already exists
         self.connect_db("aggieeats") #connect to aggieeats db
         if self.init_db: #create tables/schema along with DB
-            cmd = ''
-            with open('..\\lib\\sql.txt', 'r') as file:
-                cmd += file.read().replace('\n', '') #append all 7 CREATE TABLE commands to cmd
-            self.execute_query(cmd)
+            self.populate_db()
+    
+    def populate_db(self):
+        cmd = ''
+        with open('..\\lib\\sql.txt', 'r') as file:
+            cmd += file.read().replace('\n', '') #append all 7 CREATE TABLE commands to cmd
+        self.execute_query(cmd)
+    
+    def drop_db(self):
+        self.connect_db("postgres")
+        self.execute_query("DROP DATABASE aggieeats;")
+        exit(0)
 
     def execute_query(self,command):
         try:
             self.cursor.execute(command)
+            self.get_results()
         except Exception as err:
             if type(err) == psycopg2.errors.DuplicateDatabase: #check for preexisting DB to see if new tables need to be added
                 self.init_db = False
@@ -44,11 +53,11 @@ class backend:
         try:  
             self.results = self.cursor.fetchall()
         except Exception as err:
-            print(err)
+            pass
+            #print(err)
     
     def print_query(self):
-        try:  
-            self.get_results()
+        try:
             for r in self.results:
                 print(r)
         except Exception as err:
@@ -56,19 +65,19 @@ class backend:
 
     def insert(self,table,cols,vals):
         l,v = len(cols),len(vals)
+        sql = ["INSERT INTO " + table, " (",") VALUES (",");"]
         assert(l == v)
+        for i in range(0,l):
+            val = vals[i]
+            if type(val) == str:
+                val = "\'" + val + "\'"
+            tmp = [str(cols[i]),str(val)]
+            if i < l-1:
+                tmp = [col + ", " for col in tmp]
+            sql[1] += tmp[0]
+            sql[2] += tmp[1]
         try:
-            command = ["INSERT INTO " + table, " (",") VALUES (",");"]
-            for i in range(0,l):
-                val = vals[i]
-                if type(val) == str:
-                    val = "\'" + val + "\'"
-                tmp = [str(cols[i]),str(val)]
-                if i < l-1:
-                    tmp = [col + ", " for col in tmp]
-                command[1] += tmp[0]
-                command[2] += tmp[1]
-            self.cursor.execute("".join(command))
+            self.cursor.execute("".join(sql))
         except Exception as err:
             print(err) 
  
@@ -87,7 +96,6 @@ class backend:
         sql = "SELECT * FROM creator WHERE username = '" + username +"' AND password = '" + self.hash(password) + "';"
         try:
             self.execute_query(sql)
-            self.get_results()
             if self.results == []:
                 print("Password incorrect") 
             else:
@@ -98,10 +106,9 @@ class backend:
 
     def register(self,username,password,firstname,lastname): #takes in username and password entered into fields, checks if valid length, checks if already exists
         new_username = True
-        try:#check if username exists block
-            sql = "SELECT * FROM creator WHERE username = '" +  username + "';"
-            self.execute_query(sql)
-            self.get_results()
+        sql = "SELECT * FROM creator WHERE username = '" +  username + "';"
+        try:
+            self.execute_query(sql) #check if username exists
             if self.results == []:
                 print("Eligible username")
             else:
@@ -120,4 +127,5 @@ class backend:
 b = backend()
 b.register("user123","password1","user","name")
 b.login("user123","password1")
+b.drop_db()
 b.conn.close()
